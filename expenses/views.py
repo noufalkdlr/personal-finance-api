7f9a10a2cf31f716162dbd07628b4eca6b59c74b
Noufal <noufalkakdlr@gmail.com>
2026-01-16 13:35:47 +0530

    Create api

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryListView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
