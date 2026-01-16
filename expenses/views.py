76b850437f83b9a51d61c9866e0606019ba71200
Noufal <noufalkakdlr@gmail.com>
2026-01-16 14:41:52 +0530

    create authentication

--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,15 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework import permissions
 from .models import Category
 from .serializers import CategorySerializer
 
 
 class CategoryListView(ListCreateAPIView):
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
